7528b742f2fef1a7d1ee287449e17dfb59cf59dc
Daejong Bang <bangsmac@Daejongui-MacBookPro.local>
2019-02-06 16:01:28 +0900

    start react study

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const text = 'Are you awesome?';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+      <div>
+        <h1>hello react!</h1>
+        <h2>{text}</h2>
       </div>
     );
   }
