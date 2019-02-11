b06f291925da2c143f921db25bece731e8dfcfdd
hanasari <hanasari@stunitas.com>
2019-02-11 10:05:25 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-// import MyComponent from './MyComponent';
-// import EventPractice from './EventPractice';
-// import ValidationSample from './ValidationSample';
-// import ScrollBox from './ScrollBox';
-// import IterationSample from './IterationSample';
-import LifeCycleSample from './LifeCycleSample';
-// import PropTypes from 'prop-types';
+import logo from './logo.svg';
 import './App.css';
 
 function getRandomColor(){
@@ -13,43 +7,18 @@ function getRandomColor(){
 }
 
 class App extends Component {
-  state = {
-    color: '#000000'
-  }
-
-  handleClick = ()=>{
-    this.setState({
-      color: getRandomColor()
-    });
-  }
-
   render() {
     return (
-      <div>
-        {/* <MyComponent name="React" age={4} />
-
-        <EventPractice />
-
-        <ValidationSample />
-
-        <ScrollBox ref={(ref)=>this.scrollBox=ref} />
-        <button onClick={()=>this.scrollBox.scrollToBottom()}>to Bottom</button>
-
-        <IterationSample /> */}
-
-        <LifeCycleSample color={this.state.color} />
-        <button onClick={this.handleClick}>random color</button>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="log" />
+          <h2>welcome to React</h2>
+        </div>
+        <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.</p>
       </div>
     );
   }
 }
-
-// MyComponent.defaultProps = {
-//   name: 'default name'
-// }
-// MyComponent.propTypes = {
-//   name: PropTypes.string,
-//   age: PropTypes.number.isRequired
-// }
 
 export default App;
